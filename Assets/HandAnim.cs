e8095f1cb854fb2ed3239f29a497ee4f7b2cd265
Mark Dziuban <markdziuban@gmail.com>
2021-07-03 11:02:00 -0400

    Initial Commit

---
+++ Assets/HandAnim.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class HandAnim : MonoBehaviour
+{
+  [SerializeField] InputDeviceCharacteristics controllerCharacteristics;
+    [SerializeField] List<GameObject> controllerPrefab;
+    [SerializeField] GameObject handModelPrefab;
+    [SerializeField] bool showController = false;
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefab.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else 
+            {
+                Debug.LogError("Did not find corresponding controller model");
+                Instantiate(controllerPrefab[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+    }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else 
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void Update()
+    {
+        //targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        //targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        //targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2dAxisValue);
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            if (showController)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedController.SetActive(true);
+            }
+            else
+            {
+                spawnedHandModel.SetActive(true);
+                spawnedController.SetActive(false);
+                UpdateHandAnimation();
+            }
+        }
+
+    }
+}
